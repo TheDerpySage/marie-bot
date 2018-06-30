f5f35e3cef239199c052a0dd8972b845191c6d1b
Majora DeCamp <majoradecamp@gmail.com>
2018-06-29 19:05:16 -0500

    Another Round of Server.py Updates
    Smash Dependencies Edition
    Shortened getting the current IP
    Made socket_test its own function
    Streamlining brought to you by a future project :^)

--- README.md
+++ README.md
@@ -13,14 +13,9 @@ pip install discord.py
 
 pip install weather-api
 
-pip install beautifulsoup4
-
 pip install ping3
 
 NOTE
 =============
 The "temp.py" files must be edited and the underscore temp removed from the
-name before the application will run. Some of the emotes in assets aren't used.
-Honestly, they should be removed but I'm a bit too lazy. There is one cog not
-included in this public repo, but that's only because it contains some info about
-my LAN.
+name before the application will run.
--- server.py
+++ server.py
@@ -1,9 +1,6 @@
 import discord
 from discord.ext import commands
-#HTML Scraping
-import urllib.request
-from bs4 import BeautifulSoup
-#Ping Tool
+import urllib2
 from ping3 import ping
 import socket
 
@@ -12,7 +9,7 @@ def string_ping(host):
     if response == None:
         return (host + " is unreachable.")
     else:
-        return (host + " is reachable. (%sms)" % round(response, 2))
+        return (host + " is reachable. (%sms)" % round(response * 1000))
 
 def bool_ping(host):
     response = ping(host)
@@ -20,6 +17,13 @@ def bool_ping(host):
         return False
     else:
         return True
+
+def socket_test(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    temp = sock.connect_ex((host, port))
+    if temp == 0: sock.shutdown(socket.SHUT_RDWR)
+    sock.close()
+    return temp
 
 class ServerCog:
     '''Server functions'''
@@ -30,75 +34,68 @@ class ServerCog:
     @commands.command(pass_context = True)
     async def current_ip(self, ctx):
         """Returns Current Public IP"""
-        await self.bot.send_typing(ctx.message.channel)
-        html = urllib.request.urlopen(urllib.request.Request("http://ip.42.pl/raw")).read()
-        soup = BeautifulSoup(html, "html.parser")
-        curIP = soup.get_text()
+        curIP = urllib2.urlopen('http://ip.42.pl/raw').read()
         await self.bot.say(curIP)
 
     @commands.command(pass_context = True)
     async def services(self, ctx):
         """Service Status"""
         await self.bot.send_typing(ctx.message.channel)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #Check Toad-Town
+        result = socket_test('192.168.0.13',80)
+        if result == 0:
+            toadHealth = ":white_check_mark:"
+            public = True
+        else:
+            toadHealth = ":no_entry:(%s)" % result
+            public = False
         #Check Web
-        result = sock.connect_ex(('192.168.0.8',80))
+        result = socket_test('192.168.0.8',80)
         if result == 0:
             webHealth = ":white_check_mark:"
-            sock.shutdown(socket.SHUT_RDWR)
+            if public == False:
+                webHealth = ":warning:"
         else: webHealth = ":no_entry:(%s)" % result
         #Check Plex
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex(('192.168.0.8',32400))
+        result = socket_test('192.168.0.8',32400)
         if result == 0:
             plexHealth = ":white_check_mark:"
-            sock.shutdown(socket.SHUT_RDWR)
         else: plexHealth = ":no_entry:(%s)" % result
         #Check TF2
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex(('192.168.0.8',27015))
+        result = socket_test('192.168.0.8',27015)
         if result == 0:
             tfHealth = ":white_check_mark:"
             sock.shutdown(socket.SHUT_RDWR)
         else: tfHealth = ":no_entry:(%s)" % result
         #Check Minecraft
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex(('192.168.0.8',25565))
+        result = socket_test('45.55.104.100',25565)
         if result == 0:
             mineHealth = ":white_check_mark:"
-            sock.shutdown(socket.SHUT_RDWR)
         else: mineHealth = ":no_entry:(%s)" % result
-        #Check Toad-Town
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex(('192.168.0.13',80))
-        if result == 0:
-            toadHealth = ":white_check_mark:"
-            sock.shutdown(socket.SHUT_RDWR)
-        else: toadHealth = ":no_entry:(%s)" % result
         #Check Flower-Fields
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex(('192.168.0.12',80))
+        result = socket_test('192.168.0.12',80)
         if result == 0:
             flowerHealth = ":white_check_mark:"
-            sock.shutdown(socket.SHUT_RDWR)
+            if public == False:
+                flowerHealth=":warning:"
         else: flowerHealth = ":no_entry:(%s)" % result
         #Check Poshley-Heights
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex(('192.168.0.8',8083))
+        result = socket_test('192.168.0.8',8083)
         if result == 0:
             poshHealth = ":white_check_mark:"
-            sock.shutdown(socket.SHUT_RDWR)
+            if public == False:
+                poshHealth = ":warning:"
         else: poshHealth = ":no_entry:(%s)" % result
-        sock.close()
         #Output
-        await self.bot.say("**SERVICES**" +
+        temp = ("**SERVICES**" +
         "\n\n~Website~ " + "\nStatus:\t" + webHealth + "\nhttp://www.thederpysage.com/" +
-        "\n\n~Plex~" + "\nStatus:\t" + plexHealth + "\nhttp://plex.thederpysage.com/web/" +
+        "\n\n~Plex~" + "\nStatus:\t" + plexHealth + "\n*Use their Website*" +
         "\n\n~Poshley Heights~" + "\nStatus:\t" + poshHealth + "\nhttp://poshley-heights.thederpysage.com/" +
         "\n\n~Flower Fields~" + "\nStatus:\t" + flowerHealth + "\n*Invite Only*" +
         "\n\n~Toad Town~" + "\nStatus:\t" + toadHealth + "\n*Reverse Proxy*" +
         "\n\n~Team Fortress 2 / Garry's Mod~" + "\nStatus:\t" + tfHealth + "\nsrc.thederpysage.com" +
-        "\n\n~Minecraft~" + "\nStatus:\t" + mineHealth + "\nminecraft.thederpysage.com")
+        "\n\n~Minecraft~" + "\nStatus:\t" + mineHealth + "\n45.55.104.100:25565")
+        await self.bot.say(temp)
 
     @commands.command(pass_context = True)
     async def servers(self, ctx):
@@ -113,15 +110,19 @@ class ServerCog:
         if bool_ping("shimakaze.home"):
             shimakaze = ":white_check_mark:"
         else : shimakaze = ":no_entry:"
-        await self.bot.say("**SERVERS**" +
+        temp = ("**SERVERS**" +
         "\nNagato:\t" + nagato +
         "\nHatsuharu:\t" + hatsuharu +
-        "\nShimakaze:\t" + shimakaze)
+        "\nShimakaze:\t" + shimakaze +
+        "\nMutsu:\t" + mutsu)
+        await self.bot.say(temp)
 
-    @commands.command(pass_context = True)
+    @commands.command(aliases=['ping'], pass_context = True)
     async def bot_ping(self, ctx, hostname):
         """Pings a given host to check if it's reachable"""
-        self.bot.say(string_ping(hostname))
+        await self.bot.send_typing(ctx.message.channel)
+        await self.bot.say(string_ping(hostname))
+
 
 def setup(bot):
     bot.add_cog(ServerCog(bot))
